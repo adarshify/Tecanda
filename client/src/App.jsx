4d179efa4d52743400d0493b586a0d9996fdcd85
Adarsh <adarshchourasia6996@gmail.com>
2024-07-13 05:02:42 +0530

    integration client and server together

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useMemo, useState } from 'react';
+import {io} from "socket.io-client";
+import { Button , Container, TextField, Typography } from "@mui/material";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const socket = useMemo(()=> io("http://localhost:3000"),[]);
+
+  const [message, setMessage] = useState("");
+  const [room, setRoom] = useState("");
+
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    socket.emit("message", message);
+    setMessage("");
+  };
+
+  useEffect(()=>{
+    socket.on("connect", ()=>{
+      console.log("connected", socket.id);
+      
+    });
+
+
+
+      socket.on("receive-message", (data)=>{
+              console.log(data);
+      });
+
+     socket.on("Welcome", (s)=>{
+      console.log(s);
+     });
+
+     return () =>{
+      socket.disconnect();  
+     }
+
+  }, []);
+
+
+
+
+  return  <Container maxWidth= "sm">
+    <Typography varient="h1" component="div" gutterBottom>
+      
+      Welcome to Tecanda!
+      </Typography> 
+      <form onSubmit={handleSubmit}> 
+        <TextField value={message}
+        onChange={(e)=>setMessage(e.target.value)} 
+        id='outlined-basic'
+         label="Wisper"
+          variant='outlined'
+          />
+
+
+        <TextField value={message}
+        onChange={(e)=>setMessage(e.target.value)} 
+        id='outlined-basic'
+         label="Shout"
+          variant='outlined'
+          />
+        <Button type='submit' variant='contained' color='primary'>Send</Button>
+      </form>
+  </Container>
 }
 
 export default App
